9dbf3055fd75ae0eb2372367868f91246e0895fa
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2025-01-03 15:13:14 -0800

    formatting

--- src/app/page.js
+++ src/app/page.js
@@ -11,17 +11,17 @@ export default function Home() {
         <Col xs={12} lg={8} className="pt-lg-5">
           <h1 className="mt">Celebrating the Life of Dave LeWarne</h1>
           <div className="mb-2">
-            <div><span className="fw-bold">Date:</span> Friday January 10th 2025</div>
+            <div><span className="fw-bold">Date:</span> Friday, January 10th 2025</div>
             <div><span className="fw-bold">Time:</span> 4:00pm</div>
             <div><span className="fw-bold">Location:</span> Snohomish High School Gym (Keith Gilberston Sr. Sports and Fitness Center), 1316 5th St, Snohomish, WA 98290</div>
             <div>Find a map of the high school and parking options <a href="/img/SHS-SiteMap2.png">here</a></div>
           </div>
-          <div>There will be a livestream of the service. More information and link coming soon.</div>
+          {/* <div>There will be a livestream of the service. More information and link coming soon.</div> */}
           <a href="/livestream"><button className="btn btn-success mt-3 w-fit">Link to the Livestream</button></a>
           <h3>Submit Photos and Stories</h3>
           <div><a href="https://form.jotform.com/243655021912047">Click here</a> to submit your favorite Dave pictures and memories. After review, your submissions will be available to view on the <a href="/memories">memories</a> page.</div>
           <h2 className="mt-5">How to Support</h2>
-          <p>We encourage you to show your love for Dave through championing the kids in your community. In lieu of flowers, please consider a donation to the David J. LeWarne Memorial Scholarship</p>
+          <p>We encourage you to show your love for Dave through championing the kids in your community. In lieu of flowers, please consider a donation to the David J. LeWarne Memorial Scholarship.</p>
           <p><a href="https://www.snoed.org/donate-online/">Click here</a> to donate online, include David J LeWarne in the "Notes/Name of Tribute/Fund/Scholarship" section of the form.</p>
           <p>You can also mail a check to PO Box 1312, Snohomish, WA 98291. Please include a note for Dave LeWarne Scholarship on the check. </p>
           <h2 className="mt-5">About Dave</h2>
