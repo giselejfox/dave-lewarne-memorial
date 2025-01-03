057d5ce3106294f06ab803fd4b68adbea4b890c7
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2025-01-03 14:22:14 -0800

    parking and witing

--- src/app/page.js
+++ src/app/page.js
@@ -16,13 +16,14 @@ export default function Home() {
             <div><span className="fw-bold">Date:</span> Friday January 10th 2025</div>
             <div><span className="fw-bold">Time:</span> 4:00pm</div>
             <div><span className="fw-bold">Location:</span> Snohomish Highschool Gym, 1316 5th St, Snohomish, WA 98290</div>
+            <div>Find a map of the highschool and parking options <a href="/img/SHS-SiteMap2.png">here</a></div>
           </div>
-          <div>The livestream will commence at 4:30pm(?). After the event we will link out to a recording of the service.</div>
+          <div>There will be a livestream of the service. More information and link coming soon.</div>
           <button className="btn btn-success mt-3 mb-5 w-fit">Link to the Livestream</button>
           <h2>How to Support</h2>
           <h3>Donations</h3>
-          <p>We encourage you to show your love for Dave through championing the kids in your community. Instead of buying flowers or food, we kindly ask that you donate to the Dave LeWarne scholarship.</p>
-          <p><a href="https://www.snoed.org/donate-online/">Click here</a> to donate online, include Dave LeWarne in the "Notes/Name of Tribute/Fund/Scholarship" section of the form.</p>
+          <p>We encourage you to show your love for Dave through championing the kids in your community. Instead of buying flowers or food, we kindly ask that you donate to the David J LeWarne Memorial Scholarship.</p>
+          <p><a href="https://www.snoed.org/donate-online/">Click here</a> to donate online, include David J LeWarne in the "Notes/Name of Tribute/Fund/Scholarship" section of the form.</p>
           <p>You can also mail a check to PO Box 1312, Snohomish, WA 98291. Please include a note for Dave LeWarne Scholarship on the check. </p>
           <h3>Submit Photos and Stories</h3>
           <div><a href="https://form.jotform.com/243655021912047">Click here</a> to submit your favorite Dave pictures and memories. After review, your submissions will be shown on the <a href="/memories">memories</a> page. If you submit photos before the memorial service they will be used in the slideshow.</div>
