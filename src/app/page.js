7518189c3f0a6e5e7e32d105cd609e98e490a3eb
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2025-01-03 16:34:44 -0800

    copy change

--- src/app/page.js
+++ src/app/page.js
@@ -19,11 +19,12 @@ export default function Home() {
           {/* <div>There will be a livestream of the service. More information and link coming soon.</div> */}
           <a href="/livestream"><button className="btn btn-success mt-3 w-fit">Link to the Livestream</button></a>
           <h2 className="mt-5">Submit Photos and Stories</h2>
-          <div><a href="https://form.jotform.com/243655021912047">Click here</a> to submit your favorite Dave pictures and memories. After review, your submissions will be available to view on the <a href="/memories">memories</a> page.</div>
+          <div><a href="https://form.jotform.com/243655021912047">Click here</a> to submit your favorite pictures and memories of Dave. After review, your submissions will be available to view on the <a href="/memories">memories</a> page.</div>
           <h2 className="mt-5">How to Support</h2>
-          <p>We encourage you to show your love for Dave through championing the kids in your community. In lieu of flowers, please consider a donation to the David J. LeWarne Memorial Scholarship.</p>
+          <p>We encourage you to show your love for Dave through supporting the students in your community. In lieu of flowers, please consider a donation to the David J. LeWarne Memorial Scholarship.</p>
           <p><a href="https://www.snoed.org/donate-online/">Click here</a> to donate online, include David J LeWarne in the "Notes/Name of Tribute/Fund/Scholarship" section of the form.</p>
           <p>You can also mail a check to PO Box 1312, Snohomish, WA 98291. Please include a note for Dave LeWarne Scholarship on the check. </p>
+          <p>Thank you for your consideration.</p>
         </Col>
       </Row>
     </div>
